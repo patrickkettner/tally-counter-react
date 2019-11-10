92d28a990c8cad51746cfa6542fa12b76f3150c9
David Čáp <capdavidd@gmail.com>
2019-11-10 17:46:41 +0100

    extracted header to a component

--- src/App.js
+++ src/App.js
@@ -8,17 +8,11 @@ import { increaseCommands, decreaseCommands } from './commands.js';
 
 import { light, dark } from './theme';
 import Container from './components/Container';
+import Header from './components/Header';
+import ImageButton from './components/ImageButton';
 import Item from './components/Item';
 import Logo from './components/Logo';
 import TextButton from './components/TextButton';
-import ImageButton from './components/ImageButton';
-
-const Header = styled.h1`
-    display: inline;
-    font-size: 1.3rem;
-    font-weight: lighter;
-    transition: color 0.1s;
-`;
 
 /* global chrome */
 
@@ -97,7 +91,6 @@ class App extends Component {
     addItemHandler = () => {
         const updatedState = this.getState();
         updatedState.items.push({ itemName: '', number: 0, id: uuid() });
-
         this.setState(updatedState);
     };
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Header = styled.h1`
+    display: inline;
+    font-size: 1.3rem;
+    font-weight: lighter;
+    transition: color 0.1s;
+`;
+
+export default Header;
